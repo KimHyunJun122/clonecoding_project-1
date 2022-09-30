f6a3b58214ec9fe3cbdcefda79d4542174eae600
hyunjoojung <hyunjoo3107@gmail.com>
2022-09-30 11:13:37 +0900

    url 연결

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Instagram',
+    'content',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
